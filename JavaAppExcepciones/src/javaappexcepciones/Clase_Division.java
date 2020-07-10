01b17e431ded6ffd21e6c00f8fa5d366f5419914
Bryner <Bryner@192.168.1.11>
2020-07-09 19:21:38 -0600

    Ejemplo de excepciones

---
+++ JavaAppExcepciones/src/javaappexcepciones/Clase_Division.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaappexcepciones;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Bryner
+ */
+public class Clase_Division {
+  public static void main (String[] args){
+      try {
+          int Valor1, Valor2, vresultado;
+    Scanner entrada = new Scanner (System.in);
+    
+    System.out.print("Primer valor:    ");
+    
+        Valor1 = entrada.nextInt();
+    
+    System.out.print("Primer valor:    ");
+    
+        Valor2 = entrada.nextInt();
+    
+    vresultado = Valor1 / Valor2;
+    
+    System.out.print("resultado es : " + vresultado );
+          
+      } catch (Exception e) {
+          System.out.print("ERROR" + e);
+      }
+      
+  }
+  
+}
